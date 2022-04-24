8a6f0408459f977072e9a2495ccb07d521799609
unknown <saber.jafari.rendi@gmail.com>
2022-04-24 13:02:50 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Card from "./pages/Card";
+import { Routes, Route, Link } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ paddingBottom: "20px" }}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product-list" element={<ProductList />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/card" element={<Card />} />
+      </Routes>
+      {/* <Home /> */}
+      {/* <ProductList />
+      <Product />
+      <Login />
+      <Register />
+      <Card /> */}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/components/Navbar.js
@@ -0,0 +1,112 @@
+import { Search, ShoppingCartOutlined } from "@mui/icons-material";
+import { Badge } from "@mui/material";
+import { height } from "@mui/system";
+import React from "react";
+import styled from "styled-components";
+import { mobile } from "../../Responsive";
+import { Link } from "react-router-dom";
+
+const Container = styled.div`
+  height: 60px;
+  ${mobile({ height: "50px" })}
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  ${mobile({ padding: "10px 0px" })}
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+  ${mobile({ display: "none" })}
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+  ${mobile({ width: "50px" })}
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+  ${mobile({ fontSize: "24px" })}
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  ${mobile({ flex: 2, justifyContent: "center" })}
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+  ${mobile({ fontSize: "12px", marginLeft: "10px" })}
+`;
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
+function Navbar() {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input placeholder="Search" />
+            <Search style={{ color: "gray", fontSize: "16px" }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          {" "}
+          <Logo>
+            {" "}
+            <StyledLink to="/">SaBer. </StyledLink>{" "}
+          </Logo>{" "}
+        </Center>
+        <Right>
+          <MenuItem>
+            <StyledLink to="/register">Register</StyledLink>
+          </MenuItem>
+          <MenuItem>
+            <StyledLink to="/login">Sign In </StyledLink>
+          </MenuItem>
+
+          <MenuItem>
+            <StyledLink to="/card">
+              <Badge badgeContent={4} color="primary">
+                <ShoppingCartOutlined />
+              </Badge>{" "}
+            </StyledLink>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+}
+
+export default Navbar;
